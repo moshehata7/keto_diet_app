9237630207b1e2d3a81803601e63eb5579ff0da6
mohamed shehata <moshehata7272d@gmail.com>
2025-09-07 04:20:16 -0700

    finish calculator screen

--- lib/presentation/pages/home_screen.dart
+++ lib/presentation/pages/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:diey_app/presentation/widgets/image_category.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,8 +7,55 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("الرئيسية "),
+      appBar: AppBar(title: Text("الرئيسية ")),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            ImageCategory(
+              onTap: () {},
+              hight: 150,
+              width: double.infinity,
+              image: "images/ideal-protein-diet-VA-768x512.jpg",
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: ImageCategory(
+                    onTap: () {},
+                    hight: 120,
+                    image: "images/IntermittentFasting_final-0b2134c18e154613b64a21a48b5b0114.jpg",
+                  ),
+                ),
+                Expanded(
+                  child: ImageCategory(
+                    onTap: () {},
+                    hight: 120,
+                    image: "assets/images/ideal-protein-diet-VA-768x512.jpg",
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: ImageCategory(
+                    onTap: () {},
+                    hight: 120,
+                    image: "assets/images/ideal-protein-diet-VA-768x512.jpg",
+                  ),
+                ),
+                Expanded(
+                  child: ImageCategory(
+                    onTap: () {},
+                    hight: 120,
+                    image: "assets/images/ideal-protein-diet-VA-768x512.jpg",
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/presentation/widgets/image_category.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class ImageCategory extends StatelessWidget {
+  const ImageCategory({
+    super.key,
+     this.hight,
+     this.width,
+    required this.image, required this.onTap,
+  });
+  final double? hight, width;
+  final String image;
+  final Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: GestureDetector(
+        onTap: onTap,
+        child: Container(
+          height: hight,
+          width: width,
+          decoration: BoxDecoration(
+            image: DecorationImage(fit: BoxFit.cover, image: AssetImage(image)),
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+      ),
+    );
+  }
+}
