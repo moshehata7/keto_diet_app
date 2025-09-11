07c8dc37c408e373fdaaa3bba197cb39de0ed869
mohamed shehata <moshehata7272d@gmail.com>
2025-09-11 01:45:59 -0700

    refactor recipe screen

--- lib/presentation/pages/recipe_screen.dart
+++ lib/presentation/pages/recipe_screen.dart
@@ -3,27 +3,77 @@ import 'package:diey_app/presentation/widgets/recipe_category.dart';
 import 'package:flutter/material.dart';
 
 class RecipeScreen extends StatelessWidget {
-  const RecipeScreen({super.key});
+  RecipeScreen({super.key});
+  final List<RecipeCategory> recipes = [
+    RecipeCategory(
+      recipeName: "recipeName",
+      recipeImage: "recipeImage",
+      onTap: () {},
+    ),
+    RecipeCategory(
+      recipeName: "recipeName",
+      recipeImage: "recipeImage",
+      onTap: () {},
+    ),
+    RecipeCategory(
+      recipeName: "recipeName",
+      recipeImage: "recipeImage",
+      onTap: () {},
+    ),
+    RecipeCategory(
+      recipeName: "recipeName",
+      recipeImage: "recipeImage",
+      onTap: () {},
+    ),
+    RecipeCategory(
+      recipeName: "recipeName",
+      recipeImage: "recipeImage",
+      onTap: () {},
+    ),
+    RecipeCategory(
+      recipeName: "recipeName",
+      recipeImage: "recipeImage",
+      onTap: () {},
+    ),
+    RecipeCategory(
+      recipeName: "recipeName",
+      recipeImage: "recipeImage",
+      onTap: () {},
+    ),
+    RecipeCategory(
+      recipeName: "recipeName",
+      recipeImage: "recipeImage",
+      onTap: () {},
+    ),
+    RecipeCategory(
+      recipeName: "recipeName",
+      recipeImage: "recipeImage",
+      onTap: () {},
+    ),
+    RecipeCategory(
+      recipeName: "recipeName",
+      recipeImage: "recipeImage",
+      onTap: () {},
+    ),
+    RecipeCategory(
+      recipeName: "recipeName",
+      recipeImage: "recipeImage",
+      onTap: () {},
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("وصفات ")),
-      body: GridView(
+      body: GridView.builder(
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
         ),
-        children: [
-          RecipeCategory(
-            recipeName: "بيتزا للفطار",
-             recipeImage:"images/pizza.jpg",
-             onTap: () {
-               
-             },
-             ),
-          
-          
-        ],
+        itemCount: recipes.length,
+        itemBuilder: (context, index) {
+          return recipes[index];
+        },
       ),
     );
   }
