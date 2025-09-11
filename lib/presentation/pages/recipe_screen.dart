941af31f21027e963c75ca37659d5a3a2badf4fd
mohamed shehata <moshehata7272d@gmail.com>
2025-09-11 04:38:52 -0700

    edit recipes list

--- lib/presentation/pages/recipe_screen.dart
+++ lib/presentation/pages/recipe_screen.dart
@@ -6,17 +6,25 @@ class RecipeScreen extends StatelessWidget {
   RecipeScreen({super.key});
   final List<Recipe> recipes = [
     Recipe(name: "بيتزا للفطار", image: "images/pizza.jpg", onTap: () {}),
-    Recipe(name: "فاصوليا باللحم", image: "images/keto-green-beans.jpg", onTap: () {}),
-    Recipe(name: "كوكيز كيتو ", image: "images/15-Featured-healthy.jpg", onTap: () {}),
+    Recipe(
+      name: "فاصوليا باللحم",
+      image: "images/keto-green-beans.jpg",
+      onTap: () {},
+    ),
+    Recipe(
+      name: "كوكيز كيتو ",
+      image: "images/15-Featured-healthy.jpg",
+      onTap: () {},
+    ),
     Recipe(name: "فاهيتا لحم", image: "images/fahita.jpg", onTap: () {}),
     Recipe(name: "خبز سمسم ", image: "images/khobz.jpg", onTap: () {}),
     Recipe(name: "خبز قريش", image: "images/khobzkresh.jpg", onTap: () {}),
+    Recipe(name: "بشاميل كيتو", image: "images/photo.jpg", onTap: () {}),
     Recipe(name: "مسقعه كيتو", image: "images/meskaa.jpg", onTap: () {}),
     Recipe(name: "محشي كيتو", image: "images/mahshi.jpg", onTap: () {}),
-    Recipe(name: "بيض أومليت", image: "images/pizza.jpg", onTap: () {}),
-    Recipe(name: "بيض أومليت", image: "images/pizza.jpg", onTap: () {}),
-    Recipe(name: "بيض أومليت", image: "images/pizza.jpg", onTap: () {}),
-    Recipe(name: "بيض أومليت", image: "images/pizza.jpg", onTap: () {}),
+    Recipe(name: "كريب حلو", image: "images/crepe.jpg", onTap: () {}),
+    Recipe(name: " كفتة لحم", image: "images/kofta.jpg", onTap: () {}),
+    Recipe(name: "كيك شكولاتة", image: "images/Cake_1.webp", onTap: () {}),
   ];
 
   @override
